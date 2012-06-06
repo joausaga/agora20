411265259ff39077ba431de796904c9d258fe310
Jorge Saldivar <jorgesaldivar@gmail.com>
2012-06-06 07:11:30 +0000

    Add the schema to construct the database tables according to the model.

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table idea (
+  id                        integer auto_increment not null,
+  id_scale                  bigint,
+  date                      date,
+  title                     varchar(255),
+  content                   longtext,
+  score                     integer,
+  author                    varchar(255),
+  constraint pk_idea primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table idea;
+
+SET FOREIGN_KEY_CHECKS=1;
+
