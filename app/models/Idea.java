799687d76480d4db81149e451894e0f59885400c
Jorge Saldivar <jorgesaldivar@gmail.com>
2012-06-06 07:05:25 +0000

    Add new method and structure to Idea model.

--- app/models/Idea.java
+++ app/models/Idea.java
@@ -1,23 +1,62 @@
 package models;
 
-import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
 
-import play.data.validation.Constraints.Required;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
 
-public class Idea {
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+@Entity
+public class Idea extends Model {
 	
-	public Long id;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Integer id;
+	
+	public Long idScale;
+	public Date date;
+	
+	@Required
 	public String title;
+	@Lob
+	@Basic(fetch = FetchType.EAGER)
 	public String content;
 	
+	public Integer score;
+	public String author;
+	
+	public static Finder<Integer,Idea> find = new Finder<Integer, Idea>(
+		    Integer.class, Idea.class
+	);
+	
 	public static List<Idea> all() {
-		return new ArrayList<Idea>();
+		return find.all();
 	}
 	
 	public static void create(Idea idea) {
+		idea.save();
 	}
-	  
-	public static void delete(Long id) {
+	
+	public String getStringScore() {
+		return this.score.toString();
+	}
+	
+	public void updateScore(Integer value) {
+		this.score = this.score + value;
+		this.update();
+	}
+	
+	public static void delete(Integer id) {
+		find.ref(id).delete();
 	}
 }
